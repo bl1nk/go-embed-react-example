fa772c6e5f0cd90dba10f0f08ac7733875ee8da5
Markus Cisler <hello@markuscisler.com>
2021-07-10 19:17:04 +0200

    class -> className

--- front-end/src/app.jsx
+++ front-end/src/app.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import "./style.css";
 
 function App() {
-  return <div class="app">Hello, world!</div>;
+  return <div className="app">Hello, world!</div>;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
